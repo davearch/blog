688fbe180043231a05f50213a90d0467929fa8fc
David Archuleta <darchuleta@symmetrimarketing.com>
2023-04-01 15:57:10 -0600

    added js utils file

---
+++ static/js/utils.js
@@ -0,0 +1,211 @@
+/**
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡇⠀⠀⡏⢀⣼⣿⣖⠶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡟⢣⣼⠏⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣾⠈⠻⣄⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⣿⢸⡏⠁⠈⠳⡜⣧⡀⠀⠀⠀⠀⠀⠀⣴⠟⢠⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⡄⠙⢦⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡇⠀⠀⣽⠘⣧⠰⣆⠀⠹⡼⣧⡀⠀⠀⠀⢀⡾⢁⣴⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢦⠈⠧
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡇⠀⠀⣿⠀⢻⣧⡻⣏⢀⠹⣜⢿⣆⠀⣰⠟⣡⡞⠁⠀⢀⣀⣀⡀⢀⣀⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠇⠀⢀⡿⢛⣲⣛⣿⡛⠛⠀⠈⣿⡏⠛⣿⣛⠉⠉⠙⠛⣯⣥⡌⠙⢋⣩⠟⢻⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡶⠒⠋⠀⠙⣛⠛⠋⠙⢶⣶⣾⠿⣿⠀⣩⣴⠖⠲⣤⡀⠀⠀⠀⣰⠏⠀⠀⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⢾⣋⣀⣀⣠⣤⣈⠉⢉⣠⡴⠛⠉⢀⣼⣯⣀⣈⣉⣩⠭⣅⣀⣠⢤⡞⠉⠀⣀⣤⠘⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⡀⠀⢩⣥⠀⠀⠈⠉⠹⣧⣤⣤⣶⡿⠋⠀⠀⠈⣁⣠⡴⢦⣄⡀⠀⢹⠀⢰⠋⠈⡇⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣹⣶⢂⣿⣀⣶⡀⠀⠀⠀⠉⠉⠁⠀⠀⠀⠀⣾⢁⣸⣦⣼⣆⠉⠀⢸⠀⠸⣧⡼⠃⠰⣇⠀⠀⠀⠀⠀⢀⣴⢞⢹⡇⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣼⣋⣁⡀⠈⠙⢦⠀⠀⠀⠀⠀⠀⠀⠀⣤⠞⠉⠀⠀⣀⣈⠻⡄⠘⢧⡀⠀⠀⣤⡄⠙⣿⣶⠖⠒⠿⣟⣵⡟⢸⣿⣿⣿⡟⠛⠛⠋⠉⠉
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⢹⣿⣿⡇⠀⠀⢸⠇⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⢸⣿⣿⣧⡿⠀⠀⣷⠀⠀⠿⠃⠀⡿⠃⠀⠀⠀⢹⣿⡇⢸⣿⡟⡽⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣮⡛⠋⠁⢀⡴⠏⣠⢀⡄⠀⠀⠀⠀⠀⡜⢦⣀⣀⣈⣙⡿⠟⣁⠀⠀⢻⡀⠀⠀⣠⣾⠃⠀⠀⢀⣴⣿⣿⠀⡿⢿⣷⢃⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⡟⣋⡤⠞⠁⣼⠁⠀⠀⠀⠀⠀⠀⠀⠛⠪⠤⠴⠒⠋⠁⢀⠀⠈⠳⣄⢠⣟⢻⡦⠤⠞⢿⣿⣿⣿⡀⢷⠋⣠⣾⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡿⠋⠁⠀⢀⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠛⠛⠁⢀⣤⠀⢸⠋⢹⣿⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⣤⣤⠖⠋⠀⠀⠀⠀⠀⢀⣠⣴⣶⣿⣿⣿⣿⠖⠀⠀⠀⠀⠀⠀⠀⠈⠳⢤⡀⠀⠘⠟⠀⠸⡆⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⠀⠀⠀⠀⠀⢀⣴⣿⣿⠿⠛⠉⢉⣀⣤⠴⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⡀⠀⠀⠀⠰⡇⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣸⣟⣛⠛⢻⡆⠀⠀⠀⣠⣿⠛⣉⣤⣖⠚⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠦⠤⣤⣧⣴⣾⣛⣛⣻⣛⣛⣛⣛⣛⣿⣿
+⣀⠀⠀⠀⠀⠀⠀⠴⠒⠒⠒⠂⠀⠈⠉⠉⠁⠁⠀⠈⢻⡀⠀⢰⡏⠙⠋⠉⠉⠉⢻⣷⣦⣤⣤⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣤⣤⣴⣶⣾⣿⣿⠿⠍⠁⠀⢀⠀⠀⢈⣉⣉⣀⣤⠀
+⠀⠄⠀⠠⠤⠤⠤⠤⠤⠶⠦⠤⠴⠶⠶⠲⠶⠆⠀⠀⠘⠻⣦⡿⠗⠀⠀⠀⠀⠀⢺⣿⢟⣿⣿⣿⡿⠿⠿⠿⠿⠛⠛⠉⠹⢿⣿⢻⣿⣿⣿⣧⡤⠠⠤⠤⠤⠄⠀⠀⠀⠀⠀⣀⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⣰⣋⣵⣏⣹⣿⡭⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠃⣾⡉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+uTiLiTy FuNcTiOnS
+ */
+
+function makeWindowDraggable(windowElem, windowHeaderElem) {
+    let pos1 = 0,
+	pos2 = 0,
+	pos3 = 0,
+	pos4 = 0;
+
+    windowHeaderElem.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+	e.preventDefault();
+	pos3 = e.clientX;
+	pos4 = e.clientY;
+	document.onmouseup = closeDragElement;
+	document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+	e.preventDefault();
+	pos1 = pos3 - e.clientX;
+	pos2 = pos4 - e.clientY;
+	pos3 = e.clientX;
+	pos4 = e.clientY;
+	windowElem.style.top = windowElem.offsetTop - pos2 + "px";
+	windowElem.style.left = windowElem.offsetLeft - pos1 + "px";
+    }
+
+    function closeDragElement() {
+	document.onmouseup = null;
+	document.onmousemove = null;
+    }
+}
+
+
+function makeWindowResizable(windowElem, resizeHandleElem) {
+    resizeHandleElem.onmousedown = resizeMouseDown;
+
+    function resizeMouseDown(e) {
+	e.preventDefault();
+	document.onmouseup = closeResizeElement;
+	document.onmousemove = resizeElement;
+    }
+
+    function resizeElement(e) {
+	e.preventDefault();
+	let width = e.clientX - windowElem.offsetLeft;
+	let height = e.clientY - windowElem.offsetTop;
+	windowElem.style.width = width + "px";
+	windowElem.style.height = height + "px";
+    }
+
+    function closeResizeElement() {
+	document.onmouseup = null;
+	document.onmousemove = null;
+    }
+}
+
+function minimizeWindow(windowElem) {
+    windowElem.style.display = 'none';
+}
+
+function maximizeWindow(windowElem) {
+    windowElem.style.width = '100%';
+    windowElem.style.height = '100%';
+    windowElem.style.top = '0';
+    windowElem.style.left = '0';
+}
+
+function closeWindow(windowElem) {
+    windowElem.remove();
+}
+
+function toggleMaximizeWindow(windowElem, maximizeBtn) {
+    const isMaximized = windowElem.classList.contains('maximized');
+    const restoreBtn = windowElem.querySelector('button[aria-label="Restore"]');
+
+    if (isMaximized) {
+	windowElem.style.width = windowElem.originalWidth;
+	windowElem.style.height = windowElem.originalHeight;
+	windowElem.style.top = windowElem.originalTop;
+	windowElem.style.left = windowElem.orginalLeft;
+	windowElem.classList.remove('maximized');
+
+	maximizeBtn.style.display = '';
+	restoreBtn.style.display = 'none';
+    } else {
+	windowElem.originalWidth = windowElem.style.width;
+	windowElem.originalHeight = windowElem.style.height;
+	windowElem.originalTop = windowElem.style.top;
+	windowElem.originalLeft = windowElem.style.left;
+
+	windowElem.style.width = '100%';
+	windowElem.style.height = '100%';
+	windowElem.style.top = '0';
+	windowElem.style.left = '0';
+	windowElem.classList.add('maximized');
+
+	maximizeBtn.style.display = 'none';
+	restoreBtn.style.display = '';
+    }
+}
+
+function toggleMinimizeWindow(windowElem) {
+    const taskbarItem = windowElem.taskbarItem;
+
+    if (windowElem.style.display === 'none') {
+	windowElem.style.display = '';
+	taskbarItem.classList.add('active');
+    } else {
+	windowElem.style.display = 'none';
+	taskbarItem.classList.remove('active');
+    }
+}
+
+function createTaskbarItem(windowElem) {
+    const taskbar = document.querySelector('.taskbar');
+    const taskbarItem = document.createElement('div');
+    taskbarItem.classList.add('taskbar-item');
+    taskbarItem.innerText = windowElem.querySelector('.title-bar-text').innerText;
+    taskbarItem.onclick = () => toggleMinimizeWindow(windowElem);
+    taskbar.appendChild(taskbarItem);
+    windowElem.taskbarItem = taskbarItem;
+}
+
+function removeTaskbarItem(windowElem) {
+    if (windowElem.taskbarItem) {
+	windowElem.taskbarItem.remove();
+    }
+}
+
+function positionNewWindow(windowElem) {
+    const windows = document.querySelectorAll('.window');
+    const offset = 30 * (windows.length - 1);
+    windowElem.style.top = `${50 + offset}px`;
+    windowElem.style.left = `${50 + offset}px`;
+}
+
+function initWindowControls() {
+    const windows = document.querySelectorAll(".window");
+    windows.forEach((windowElem) => {
+	const windowHeaderElem = windowElem.querySelector(".title-bar");
+	const resizeHandleElem = document.createElement("div");
+	resizeHandleElem.className = "resize-handle";
+	windowElem.appendChild(resizeHandleElem);
+
+	makeWindowDraggable(windowElem, windowHeaderElem);
+	makeWindowResizable(windowElem, resizeHandleElem);
+
+	positionNewWindow(windowElem);
+	
+	const minimizeBtn = windowElem.querySelector('button[aria-label="Minimize"]');
+	const maximizeBtn = windowElem.querySelector('button[aria-label="Maximize"]');
+	const closeBtn = windowElem.querySelector('button[aria-label="Close"]');
+	const restoreBtn = document.createElement('button');
+	restoreBtn.setAttribute('aria-label', 'Restore');
+	restoreBtn.style.display = 'none';
+	windowElem.querySelector('.title-bar-controls').insertBefore(restoreBtn, closeBtn);
+	createTaskbarItem(windowElem);
+
+	if (minimizeBtn) {
+	    minimizeBtn.addEventListener('click', () => toggleMinimizeWindow(windowElem));
+	}
+
+	if (maximizeBtn) {
+	    maximizeBtn.addEventListener('click', () => toggleMaximizeWindow(windowElem, maximizeBtn));
+	}
+
+	if (restoreBtn) {
+	    restoreBtn.addEventListener('click', () => toggleMaximizeWindow(windowElem, maximizeBtn));
+	}
+
+	if (closeBtn) {
+	    closeBtn.addEventListener('click', () => {
+		closeWindow(windowElem);
+		removeTaskbarItem(windowElem);
+	    });
+	}
+    });
+}
+
+function toggleStartMenu() {
+    const startMenu = document.querySelector('.start-menu-dropdown');
+    startMenu.style.display = startMenu.style.display === 'none' ? 'block' : 'none';
+    // startMenu.classList.toggle('active');
+}
+
+document.addEventListener("DOMContentLoaded", initWindowControls);
